77ea7fefe9b7db788c6afe1f720715cd8db3480d
Danizad4 <d4nielcosta@gmail.com>
2024-12-02 19:36:47 -0300

    Iniciando os ingressos

--- museumsite/settings.py
+++ museumsite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/login/'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
--- museumsite/urls.py
+++ museumsite/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('post.urls')),  
-    path('',include('staticpages.urls'))
+    path('',include('staticpages.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/login/'), name='logout')
 ]
